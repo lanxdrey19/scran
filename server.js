ba3fb877b73a7e16a13bbb27561633f3b1cdb875
Lance Delos Reyes <lan_drey19@hotmail.com>
2025-06-05 23:04:50 +1200

    :rocket: implement web service for free-tier deployment

---
+++ server.js
@@ -0,0 +1,13 @@
+import "./index.js";
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Bot is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Web server is listening on port ${PORT}`);
+});
